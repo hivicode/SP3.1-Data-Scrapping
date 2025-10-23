570337e0c8d0288bed89ee1abb17ce0895d3f532
hivicode <bintangazzuri08@gmail.com>
2025-10-23 10:35:34 +0700

    update

--- pertemuan_6/fungsi.py
+++ pertemuan_6/fungsi.py
@@ -8,7 +8,24 @@ def write_to_file(path, data):
     with open(path,'a') as file:
         file.write(data + '\n')
 
-def read_data(path):
+def read_data(path, limit):
     with open(path,'rt') as file:
+        count = 0
         for line in file:
-            print(line.replace("\n",""))
+            if count == limit:
+                break
+            if line == "\n":
+                continue
+            else:
+                count += 1
+                print(line.replace("\n",""))
+                
+
+def does_file_exist(path):
+    return os.path.isfile(path)
+
+def remove_file(path):
+    if does_file_exist(path):
+        os.remove(path)
+    else:
+        print("file tidak ada")
